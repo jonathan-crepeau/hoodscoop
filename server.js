b135d95bae73f536643217b246c90bc234b51fd5
jonathan-crepeau <jon.crepeau@gmail.com>
2020-01-31 16:27:18 -0800

    User API Routes for login, verify, delete. Updated user signup api route to async/await and to include hash password

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-// EXTERNAL MODULES ============================= //
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const mongo = require('mongodb');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
+const app = express();
 const db = require('./models');
-// INTERNAL MODULES ============================= //
-
-// CONFIGURATION VARIABLES ============================= //
 const PORT = process.env.PORT || 4000;
 
 // MIDDLEWARE ============================= //
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+
+// Express Session 
+app.use(session({
+  store: new MongoStore({
+    url: 'mongodb://localhost:27017/user-info',
+  }),
+  secret: process.env.SESSION_SECRET || "jdugifjk24u994u8tk32ngi3u",
+  resave: false,
+  saveUninitialized: false,
+  cookie:{
+    maxAge: 1000 * 60 * 60 * 24 * 7 * 2 // two weeks
+  },
+}));
 
 // HTML ROUTES ============================= //
 
@@ -43,7 +54,18 @@ app.post('/api/test', (req, res) => {
   res.json({status: 200, message: 'Test Success'})
 });
 
-app.post('/api/submitForm', (req, res) => {
+// POST Signup (Create) Single User
+
+app.post('/api/submitForm', async (req, res) => {
+  const userData = req.body;
+  let hash;
+
+  try {
+    hash = await bcrypt.hashSync(req.body.password, 10);
+    userData.password = hash;
+  } catch (err) {
+    res.status(400).json({status: 400, error: 'Bad Request!'});
+  }
 
      console.log("in submit form")
 
@@ -53,9 +75,66 @@ app.post('/api/submitForm', (req, res) => {
        }
        console.log(`saved new user: ${savedUser}`)
        res.json({savedUser});
-     })
-  })
+     });
+  });
 
+
+// POST Login API Route
+
+app.post('/api/login', (req, res) => {
+  const { firstName, lastName, email, password} = req.body;
+
+  db.User.findOne({email}, async (err, foundUser) => {
+    let passwordsMatch;
+    if (err) res.status(400).json({status: 540, error: 'Bad request(A)'});
+    
+    if(!foundUser) {
+      return res.status(400).json({status: 400, message: 'Username or password is incorrect.'});
+    }
+
+    try {
+      passwordsMatch = await bcrypt.compare(password, foundUser.password); 
+      console.log(passwordsMatch);
+    } catch (err) {
+      res.status(400).json({status: 400, message: 'Bad request(B).'});
+    }
+
+    req.session.currentUser = foundUser._id;
+    req.session.createdAt = new Date().toDateString();
+    req.session.user = foundUser;
+
+    console.log(req.session);
+
+
+
+    if (passwordsMatch) { 
+      res.status(200).json({status: 200, message: 'Success!'});
+    } else {
+      res.status(400).json({status: 400, error: 'Invalid credentials.'});
+    }
+
+  });
+});
+
+
+// GET Verify Single User 
+
+app.get('/api/verify', (req, res) => {
+  if (!req.session.currentUser) {
+    return res.status(401).json({status: 401, error: 'Unauthorized, please login and try again.'})
+  }
+  res.status(200).json(req.session.user);
+});
+
+
+// DELETE Destroy Single User
+
+app.delete('/api/users/:id', (req, res) => {
+  db.User.findByIdAndDelete(req.params.id, (err, deleteUser) => {
+    if (err) res.status(400).json({status: 400, error: 'Bad request, please try again.'});
+    res.status(200).json(deleteUser);
+  });
+});
 
 // START SERVER ============================= //
 app.listen(PORT, () => {
