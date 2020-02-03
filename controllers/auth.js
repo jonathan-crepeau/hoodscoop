bdb24888451193f4285eb4c5c4dac5847dbe254e
jonathan-crepeau <jon.crepeau@gmail.com>
2020-02-03 09:31:48 -0800

    MVC files linked

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcryptjs');
+const db = require('../models');
+
+// USER SIGNUP API CONTROLLER
+
+
+// USER LOGIN API CONTROLLER
+
+
+// USER VERIFY API CONTROLLER
+
+
+// USER DELETE API CONTROLLER
+
+module.exports = {
+
+};
