e287ce6ee9ff71ed8df53b9dfcc62ce66fdabf6a
jonathan-crepeau <jon.crepeau@gmail.com>
2020-02-04 13:34:46 -0800

    resolved single route on server.js for api routes

--- public/scripts/logout.js
+++ public/scripts/logout.js
@@ -1,4 +1,16 @@
 
 $('#userLogout').on('click', () => {
     console.log('clicked');
+
+    function onSuccess() {
+        window.location = '/'
+    }
+
+    $.ajax({
+        method: 'DELETE',
+        url: '/api/logout',
+        success: onSuccess,
+        error: error => console.log(error),
+    });
+
 });
